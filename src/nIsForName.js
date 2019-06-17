97571a8d08e3eb22b08ba985afb5b1616159148f
Iryna <iriskag@ukr.net>
2019-06-17 16:53:13 +0300

    Solution

--- src/nIsForName.js
+++ src/nIsForName.js
@@ -22,7 +22,12 @@
  * @return {string[]}
  */
 function nIsForName(names) {
-  // write code here
+  function itemOfNames(item) {
+    if (item.split(' ').some(name => name[0] === 'N')) {
+      return item;
+    }
+  }
+  return (names.filter(item => itemOfNames(item)));
 }
 
 module.exports = nIsForName;
