f71ada9dc55fce702437c3e1bd18032895e007b0
Denys Zabudskyi <deniszabudsky@gmail.com>
2019-06-23 23:23:15 +0300

    Solution

--- src/nIsForName.js
+++ src/nIsForName.js
@@ -22,7 +22,11 @@
  * @return {string[]}
  */
 function nIsForName(names) {
-  // write code here
+  return names.filter(name => {
+    return name.split(' ').some(firstSymbol => {
+      return firstSymbol.startsWith('N');
+    });
+  });
 }
 
 module.exports = nIsForName;
