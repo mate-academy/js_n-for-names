dd6068a120e66d5d5f22c442fc49a0b996254d96
yevhen-sahach <antitop04@gmail.com>
2019-08-19 14:19:10 +0300

    first commit

--- src/nIsForName.js
+++ src/nIsForName.js
@@ -22,7 +22,7 @@
  * @return {string[]}
  */
 function nIsForName(names) {
-  // write code here
+  return names.filter(element => element[0] === 'N' || element.includes(' N'));
 }
 
 module.exports = nIsForName;
