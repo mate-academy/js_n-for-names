89c1793d84195a29b67eed7b5f7ce8a1b64d4497
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-21 13:47:17 +0200

    develop

--- src/nIsForName.js
+++ src/nIsForName.js
@@ -22,7 +22,9 @@
  * @return {string[]}
  */
 function nIsForName(names) {
-  // write code here
+  return names
+    .filter(fullname => fullname.split(' ')
+      .some(word => /^N/.test(word)));
 }
 
 module.exports = nIsForName;
