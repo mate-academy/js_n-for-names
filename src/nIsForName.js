45fade40829165b907e282c10b6bc85722c70138
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-04-06 18:15:43 +0300

    fixed

--- src/nIsForName.js
+++ src/nIsForName.js
@@ -22,7 +22,8 @@
  * @return {string[]}
  */
 function nIsForName(names) {
-  return names.filter(el => el.split(' ').some(el1 => el1[0] === 'N'));
+  return names.filter(name => name.split(' ')
+    .some(partOfName => partOfName[0] === 'N'));
 }
 
 module.exports = nIsForName;
